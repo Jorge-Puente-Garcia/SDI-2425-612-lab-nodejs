11e1eadcc997d7fd888040fc44174bd45d99c36b
Jorge-Puente-García <113630985+JorgeePG@users.noreply.github.com>
2025-03-17 20:56:49 +0100

    SDI-2425-612-Subida a ficheros y gestión de colecciones.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,4 +1,19 @@
+const {ObjectId} = require("mongodb");
 module.exports = function (app,songsRepository) {
+  app.get('/shop',function(req,res){
+    let filter = {};
+    let options = {sort: { title: 1}};
+    if(req.query.search != null && typeof(req.query.search) != "undefined" && req.query.search != ""){
+      filter = {"title": {$regex: ".*" + req.query.search + ".*"}};
+    }
+
+    songsRepository.getSongs(filter, options).then(songs =>
+    {res.render("shop.twig",{songs:songs});
+    }).catch(err =>{
+      res.send("Se ha producido un error al listar las canciones"+err)
+    });
+  })
+
   app.get("/songs", function (req, res) {
     let songs=[{
       "title":"Blank space", "price":"1.2"
@@ -14,13 +29,19 @@ module.exports = function (app,songsRepository) {
     res.send(String(response));
   });
   app.get('/songs/add', function (req, res) {
-    res.render("add.twig");
+    res.render("songs/add.twig");
   });
 
-  app.get('/songs/:id', function(req, res) {
-    let response = 'id: ' + req.params.id;
-    res.send(response);
-  });
+  app.get('/songs/:id', function (req, res) {
+    let filter = {_id: new ObjectId(req.params.id)};
+    let options = {};
+    songsRepository.findSong(filter, options).then(song => {
+      res.render("songs/song.twig", {song: song});
+    }).catch(error => {
+      res.send("Se ha producido un error al buscar la canción " + error)
+    });
+  })
+
   app.get('/songs/:kind/:id', function(req, res) {
     let response = 'id: ' + req.params.id + '<br>'
         + 'Tipo de música: ' + req.params.kind;
@@ -34,10 +55,26 @@ module.exports = function (app,songsRepository) {
       price: req.body.price
     }
     songsRepository.insertSong(song,function(result){
-      if(result.songId !== null && result.songId !== undefined){
-        res.send("Agregada la canción ID: "+result.songId);
-      }else{
-        res.send("Error al insertar canción "+result.songId);
+      if (result.songId !== null && result.songId !== undefined) {
+        if (req.files != null) {
+          let image = req.files.cover;
+          image.mv(app.get("uploadPath") + '/public/covers/' + result.songId + '.png')
+              .then(() => {
+                if (req.files.audio != null) {
+                  let audio = req.files.audio;
+                  audio.mv(app.get("uploadPath") + '/public/audios/' + result.songId + '.mp3')
+                      .then(res.send("Agregada la canción ID: " + result.songId))
+                      .catch(error => res.send("Error al subir el audio de la canción"))
+                }else {
+                  res.send("Agregada la canción ID: " + result.songId)
+                }
+              })
+              .catch(error => res.send("Error al subir la portada de la canción"))
+        } else {
+          res.send("Agregada la canción ID: " + result.songId)
+        }
+      } else {
+        res.send("Error al insertar canción " + result.error);
       }
     });
   });
