2bb3bb49d7893e6e8619df85ee68e9a14c6d6a44
Jorge-Puente-García <113630985+JorgeePG@users.noreply.github.com>
2025-03-30 21:50:54 +0200

    SDI-2425-612-11.2-SW API REST Autenticación.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -10,6 +10,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 
 var app = express();
+let jwt = require('jsonwebtoken');
+app.set('jwt', jwt);
 let expressSession = require('express-session');
 app.use(expressSession({
   secret: 'abcdefg',
@@ -50,12 +52,15 @@ app.use("/shop/",userSessionRouter)
 const userAuthorRouter = require('./routes/userAuthorRouter');
 app.use("/songs/edit",userAuthorRouter);
 app.use("/songs/delete",userAuthorRouter);
+const userTokenRouter = require('./routes/userTokenRouter');
+app.use("/api/v1.0/songs/", userTokenRouter);
+
 
 let favoriteSongsRepository = require("./repositories/favoriteSongsRepository.js");
 let songsRepository = require("./repositories/songsRepository.js");
 favoriteSongsRepository.init(app, dbClient);
 songsRepository.init(app, dbClient);
-
+require("./routes/api/songsAPIv1.0.js")(app, songsRepository, usersRepository);
 require("./routes/songs/favorites.js")(app, favoriteSongsRepository,songsRepository);
 require("./routes/songs.js")(app, songsRepository);
 require("./routes/api/songsAPIv1.0.js")(app, songsRepository);
